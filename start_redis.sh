1e011216baf815a07928d26ee87fde2639d634e0
Naqi <mohammednaqi@gmail.com>
2023-02-01 19:30:42 +0500

    Update start_redis.sh

--- start_redis.sh
+++ start_redis.sh
@@ -4,5 +4,5 @@ then
   echo 'Redis is already running'
 else
   echo 'Starting Redis'
-  screen -L -Logfile .redis_log -d -m /usr/bin/redis-server REDISDIR/redis.conf
+  screen -L -Logfile screen_redis.log -d -m /usr/bin/redis-server REDISDIR/redis.conf
 fi

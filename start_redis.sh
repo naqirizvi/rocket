0c841f32c85f4880e32ba11817459322838fb4e8
Naqi <mohammednaqi@gmail.com>
2023-02-01 16:56:24 +0500

    Create start_redis.sh

---
+++ start_redis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [[ $(/usr/bin/redis-cli -s REDISDIR/redis.sock ping 2>/dev/null) == "PONG" ]]
+then
+  echo 'Redis is already running'
+else
+  echo 'Starting Redis'
+  screen -L -Logfile .redis_log -d -m /usr/bin/redis-server REDISDIR/redis.conf
+fi
